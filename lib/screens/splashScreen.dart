ed5258c5d76474a7ed07e24935a1af3b6eef5d1f
Mouhib777 <mouhibsahbani@gmail.com>
2023-05-06 16:02:31 +0000

    cc

--- lib/screens/splashScreen.dart
+++ lib/screens/splashScreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:agri_vision/constant/constant.dart';
 import 'package:agri_vision/screens/loginScreen.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -45,22 +46,25 @@ class _splashScreenState extends State<splashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Image.asset("assets/images/splash.png"),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Empower your tree care\nwith the tap of an app',
-            textAlign: TextAlign.center,
-            style: GoogleFonts.montserrat(
-                color: primaryColor,
-                fontSize: 16,
-                fontWeight: FontWeight.w800,
-                letterSpacing: 3),
-          )
-        ]),
+      body: FadeOut(
+        delay: Duration(milliseconds: 2500),
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Image.asset("assets/images/splash.png"),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Empower your tree care\nwith the tap of an app',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.montserrat(
+                  color: primaryColor,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w800,
+                  letterSpacing: 3),
+            )
+          ]),
+        ),
       ),
     );
   }
