f5bb5f319393ae0c7ba4041afdeb1f630bd309db
Mouhib777 <mouhibsahbani@gmail.com>
2023-05-06 21:04:45 +0000

    c

--- lib/screens/palm.dart
+++ lib/screens/palm.dart
@@ -1,4 +1,5 @@
 import 'package:agri_vision/constant/constant.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -17,12 +18,37 @@ class _palmScreenState extends State<palmScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: primaryColor,
-        title: Text(
-          'Palm',
-          style: GoogleFonts.montserratAlternates(
-              letterSpacing: 2, fontSize: 22, fontWeight: FontWeight.w600),
+        title: FadeInDown(
+          child: Text(
+            'Palm',
+            style: GoogleFonts.montserratAlternates(
+                letterSpacing: 2, fontSize: 22, fontWeight: FontWeight.w600),
+          ),
         ),
         centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            FadeInLeft(child: Image.asset("assets/images/palm.jpg")),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "The Arecaceae is a family of perennial flowering plants in the monocot order Arecales. Their growth form can be climbers, shrubs, tree-like and stemless plants, all commonly known as palms. Those having a tree-like form are called palm trees.[3] Currently, 181 genera with around 2,600 species are known,[4][5] most of which are restricted to tropical and subtropical climates. Most palms are distinguished by their large, compound, evergreen leaves, known as fronds, arranged at the top of an unbranched stem. However, palms exhibit an enormous diversity in physical characteristics and inhabit nearly every type of habitat within their range, from rainforests to deserts." +
+                    "Palms are among the best known and most extensively cultivated plant families. They have been important to humans throughout much of history. Many common products and foods are derived from palms. In contemporary times, palms are also widely used in landscaping. In many historical cultures, because of their importance as food, palms were symbols for such ideas as victory, peace, and fertility. ",
+                textAlign: TextAlign.left,
+                style: GoogleFonts.montserrat(),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            )
+          ],
+        ),
       ),
     );
   }
