9e75834fe64f8f141b86894876e64dfd6faf2e04
Mouhib777 <mouhibsahbani@gmail.com>
2023-05-11 17:26:39 +0000

    c

---
+++ lib/screens/history.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class historyScreen extends StatefulWidget {
+  const historyScreen({super.key});
+
+  @override
+  State<historyScreen> createState() => _historyScreenState();
+}
+
+class _historyScreenState extends State<historyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
