6f368608f910794f3c83d04fb21d29cb637d644a
Mouhib777 <mouhibsahbani@gmail.com>
2023-05-11 20:39:26 +0000

    cc

--- lib/screens/treeScreen.dart
+++ lib/screens/treeScreen.dart
@@ -1,5 +1,7 @@
 import 'package:agri_vision/constant/constant.dart';
 import 'package:agri_vision/screens/history.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -15,6 +17,8 @@ class treeScreen extends StatefulWidget {
 }
 
 class _treeScreenState extends State<treeScreen> {
+  String dropdownValue = 'Palm';
+  String? number;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +49,7 @@ class _treeScreenState extends State<treeScreen> {
                 child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 20),
                     child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
                           SizedBox(height: 30),
                           Row(
@@ -95,6 +99,161 @@ class _treeScreenState extends State<treeScreen> {
                                 color: Colors.grey,
                               ),
                             ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            height: 50,
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return StatefulBuilder(
+                                          builder: (context, setState) {
+                                        return AlertDialog(
+                                            shape: ContinuousRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(45)),
+                                            title: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [],
+                                            ),
+                                            content: Column(
+                                              children: [
+                                                DropdownButton<String>(
+                                                  iconSize: 40,
+                                                  value: dropdownValue,
+                                                  onChanged:
+                                                      (String? newValue) {
+                                                    setState(() {
+                                                      dropdownValue = newValue!;
+                                                    });
+                                                  },
+                                                  items: <String>[
+                                                    "Palm",
+                                                    "Lemon",
+                                                    "Olive",
+                                                  ].map<
+                                                          DropdownMenuItem<
+                                                              String>>(
+                                                      (String value) {
+                                                    return DropdownMenuItem<
+                                                        String>(
+                                                      value: value,
+                                                      child: Text(
+                                                        value,
+                                                        style:
+                                                            GoogleFonts.raleway(
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .bold),
+                                                      ),
+                                                    );
+                                                  }).toList(),
+                                                ),
+                                                SizedBox(
+                                                  height: 20,
+                                                ),
+                                                TextFormField(
+                                                  onChanged: (value) {
+                                                    number = value;
+                                                  },
+                                                  decoration: InputDecoration(
+                                                    hintStyle: GoogleFonts
+                                                        .montserrat(),
+                                                    labelStyle: GoogleFonts
+                                                        .montserrat(),
+                                                    counterStyle: GoogleFonts
+                                                        .montserrat(),
+                                                    hintText: 'Number of trees',
+                                                    border: OutlineInputBorder(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(15)),
+                                                    labelText:
+                                                        '  Number of trees',
+                                                  ),
+                                                  autofocus: false,
+                                                  keyboardType:
+                                                      TextInputType.number,
+                                                ),
+                                              ],
+                                            ),
+                                            actions: <Widget>[
+                                              Center(
+                                                child: SizedBox(
+                                                  width: MediaQuery.of(context)
+                                                          .size
+                                                          .width *
+                                                      0.5,
+                                                  child: ElevatedButton(
+                                                      onPressed: () async {
+                                                        final User? _userr =
+                                                            FirebaseAuth
+                                                                .instance
+                                                                .currentUser;
+                                                        final _uid =
+                                                            _userr!.uid;
+                                                        await FirebaseFirestore
+                                                            .instance
+                                                            .collection('users')
+                                                            .doc(_uid)
+                                                            .collection(
+                                                                'my trees')
+                                                            .doc()
+                                                            .set({
+                                                          "tree type":
+                                                              dropdownValue,
+                                                          "number": number
+                                                        });
+
+                                                        Navigator.pop(context);
+                                                      },
+                                                      child: Text(
+                                                        'add tree',
+                                                        style: GoogleFonts
+                                                            .montserrat(
+                                                                color: Colors
+                                                                    .white,
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .w500,
+                                                                fontSize: 18),
+                                                      ),
+                                                      style: ElevatedButton
+                                                          .styleFrom(
+                                                        backgroundColor:
+                                                            primaryColor,
+                                                        shape: RoundedRectangleBorder(
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        35.0)),
+                                                      )),
+                                                ),
+                                              ),
+                                            ]);
+                                      });
+                                    },
+                                  );
+                                },
+                                child: Text(
+                                  'Add trees',
+                                  style: GoogleFonts.montserrat(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 18),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: primaryColor,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(35.0)),
+                                )),
                           )
                         ]))),
           )
