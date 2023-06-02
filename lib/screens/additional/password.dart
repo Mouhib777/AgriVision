56ba065fcff0e51b0f7d1f5e56c5897254a7f6ca
Mouhib777 <mouhibsahbani@gmail.com>
2023-06-02 16:40:24 +0000

    c

--- lib/screens/additional/password.dart
+++ lib/screens/additional/password.dart
@@ -1,6 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import '../../constant/constant.dart';
 
 class editPassword extends StatefulWidget {
   const editPassword({super.key});
@@ -10,6 +15,13 @@ class editPassword extends StatefulWidget {
 }
 
 class _editPasswordState extends State<editPassword> {
+  String? password;
+  bool _obscureText = false;
+  var _passwordController = TextEditingController();
+  var _confirmController = TextEditingController();
+  bool _isLoading = false;
+  String? p_confirm;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +38,7 @@ class _editPasswordState extends State<editPassword> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(25.0),
           child: Column(
             children: [
               SizedBox(
@@ -36,8 +48,126 @@ class _editPasswordState extends State<editPassword> {
                 "Enter your new password, make sure it’s easy for you to remember and secure enough to use",
                 textAlign: TextAlign.left,
                 style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.w400, fontSize: 14),
-              )
+                    fontWeight: FontWeight.w400, fontSize: 16),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  hintStyle: GoogleFonts.montserrat(),
+                  labelStyle: GoogleFonts.montserrat(),
+                  counterStyle: GoogleFonts.montserrat(),
+                  hintText: 'Password',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  labelText: '  Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                ),
+                autofocus: false,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter password';
+                  } else {
+                    setState(() {
+                      password = value;
+                    });
+                    return null;
+                  }
+                },
+                onChanged: (value) {
+                  password = value;
+                },
+                obscureText: _obscureText,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: _confirmController,
+                decoration: InputDecoration(
+                  hintStyle: GoogleFonts.montserrat(),
+                  labelStyle: GoogleFonts.montserrat(),
+                  counterStyle: GoogleFonts.montserrat(),
+                  hintText: 'Confirm Password',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  labelText: '  Confirm Password',
+                ),
+                autofocus: false,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'password not confirmed';
+                  } else if (value != _passwordController.text) {
+                    return 'Passwords do not match';
+                  } else {
+                    setState(() {
+                      p_confirm = value;
+                    });
+                    return null;
+                  }
+                },
+                onChanged: (value) {
+                  p_confirm = value;
+                },
+                obscureText: true,
+              ),
+              Text(
+                "Passwords must be at least 8 characters long (only alphabet letter, number and .!?*)",
+                style: GoogleFonts.montserrat(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    color: Color(0xFF7F7F7F)),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                width: 333,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    final _user = FirebaseAuth.instance.currentUser;
+                    final _uid = _user!.uid;
+                    if (password != null && password == p_confirm) {
+                      setState(() {
+                        _isLoading = true;
+                      });
+
+                      await FirebaseFirestore.instance
+                          .collection("users")
+                          .doc(_uid)
+                          .update({"password": password.toString()});
+                      setState(() {
+                        _isLoading = false;
+                      });
+                    } else {
+                      EasyLoading.showError('Nothing to update');
+                    }
+                  },
+                  child: Text(
+                    "Update password",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(35.0)),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
