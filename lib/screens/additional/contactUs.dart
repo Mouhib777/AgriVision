7a0538fdc42bbe586052f8235d00a9b3e9422799
Mouhib777 <mouhibsahbani@gmail.com>
2023-06-02 18:34:05 +0000

    cc

--- lib/screens/additional/contactUs.dart
+++ lib/screens/additional/contactUs.dart
@@ -11,6 +11,9 @@ class contactUs extends StatefulWidget {
 }
 
 class _contactUsState extends State<contactUs> {
+  var phoneNumber = "tel:+21655555555";
+  var facebook = "Facebook.com/agrivision.tn";
+  var mail = "contact@agrivision.com";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,95 +31,103 @@ class _contactUsState extends State<contactUs> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 80,
-                ),
-                Text(
-                  "You can contact us through one of these chanels",
-                  style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.w400, fontSize: 14),
-                ),
-                SizedBox(
-                  height: 150,
-                ),
-                Container(
-                  height: 50,
-                  width: 333,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 20,
                   ),
-                  child: Center(
-                      child: Text(
-                    "contactus@agrivision.com",
+                  Text(
+                    "You can contact us through one of these\nchanels",
                     style: GoogleFonts.montserrat(
-                        fontWeight: FontWeight.w400, fontSize: 16),
-                  )),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 50,
-                  width: 333,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                        letterSpacing: 1),
                   ),
-                  child: Center(
-                      child: Text(
-                    "+216 55 555 555",
-                    style: GoogleFonts.montserrat(
-                        fontWeight: FontWeight.w400, fontSize: 16),
-                  )),
+                ],
+              ),
+              SizedBox(
+                height: 150,
+              ),
+              Container(
+                height: 50,
+                width: 380,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 50,
-                  width: 333,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Center(
-                      child: Text(
-                    "Facebook.com/agrivision.tn",
-                    style: GoogleFonts.montserrat(
-                        fontWeight: FontWeight.w400, fontSize: 16),
-                  )),
-                ),
-                SizedBox(
-                  height: 80,
-                ),
-                SizedBox(
-                  width: 333,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () async {},
+                child: Center(
                     child: Text(
-                      "Continue",
-                      style: GoogleFonts.poppins(
-                          fontSize: 18, fontWeight: FontWeight.w600),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: primaryColor,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(35.0)),
-                    ),
+                  "contactus@agrivision.com",
+                  style: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.w400, fontSize: 16),
+                )),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 50,
+                width: 380,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Center(
+                    child: Text(
+                  "+216 55 555 555",
+                  style: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.w400, fontSize: 16),
+                )),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 50,
+                width: 380,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Center(
+                    child: Text(
+                  "Facebook.com/agrivision.tn",
+                  style: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.w400, fontSize: 16),
+                )),
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              SizedBox(
+                width: 333,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    "Continue",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(35.0)),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
