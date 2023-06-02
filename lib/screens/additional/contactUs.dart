4dcc456c923fa15075770c385a6c362670f659f3
Mouhib777 <mouhibsahbani@gmail.com>
2023-06-02 18:27:28 +0000

    c

--- lib/screens/additional/contactUs.dart
+++ lib/screens/additional/contactUs.dart
@@ -1,3 +1,4 @@
+import 'package:agri_vision/constant/constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -26,74 +27,96 @@ class _contactUsState extends State<contactUs> {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 80,
-              ),
-              Text(
-                "You can contact us through one of these chanels",
-                style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.w400, fontSize: 14),
-              ),
-              SizedBox(
-                height: 150,
-              ),
-              Container(
-                height: 50,
-                width: 333,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
                 ),
-                child: Center(
-                    child: Text(
-                  "contactus@agrivision.com",
+                Text(
+                  "You can contact us through one of these chanels",
                   style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.w400, fontSize: 16),
-                )),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 50,
-                width: 333,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
+                      fontWeight: FontWeight.w400, fontSize: 14),
                 ),
-                child: Center(
-                    child: Text(
-                  "+216 55 555 555",
-                  style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.w400, fontSize: 16),
-                )),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 50,
-                width: 333,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
+                SizedBox(
+                  height: 150,
                 ),
-                child: Center(
+                Container(
+                  height: 50,
+                  width: 333,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Center(
+                      child: Text(
+                    "contactus@agrivision.com",
+                    style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.w400, fontSize: 16),
+                  )),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 50,
+                  width: 333,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Center(
+                      child: Text(
+                    "+216 55 555 555",
+                    style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.w400, fontSize: 16),
+                  )),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 50,
+                  width: 333,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Center(
+                      child: Text(
+                    "Facebook.com/agrivision.tn",
+                    style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.w400, fontSize: 16),
+                  )),
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                SizedBox(
+                  width: 333,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () async {},
                     child: Text(
-                  "Facebook.com/agrivision.tn",
-                  style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.w400, fontSize: 16),
-                )),
-              ),
-            ],
+                      "Continue",
+                      style: GoogleFonts.poppins(
+                          fontSize: 18, fontWeight: FontWeight.w600),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: primaryColor,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(35.0)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
