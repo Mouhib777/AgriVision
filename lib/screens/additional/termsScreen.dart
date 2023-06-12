985a5f61712e85555a45c2aabba824f9223ece4a
Mouhib777 <mouhibsahbani@gmail.com>
2023-06-12 22:10:01 +0000

    c

--- lib/screens/additional/termsScreen.dart
+++ lib/screens/additional/termsScreen.dart
@@ -1,3 +1,4 @@
+import 'package:agri_vision/constant/constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -30,17 +31,10 @@ class _termsScreenState extends State<termsScreen> {
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               SizedBox(
                 height: 20,
-              ),
-              Text(
-                "Sure, here's a sample terms and conditions of use for Agrivision: ",
-                textAlign: TextAlign.left,
-                style: GoogleFonts.montserrat(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14,
-                ),
               ),
               SizedBox(
                 height: 10,
@@ -56,6 +50,31 @@ class _termsScreenState extends State<termsScreen> {
                     ),
                   ),
                 ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                para1,
+                style: GoogleFonts.montserrat(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "## User Account",
+                    style: GoogleFonts.montserrat(),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                para1,
+                style: GoogleFonts.montserrat(),
               ),
             ],
           ),
