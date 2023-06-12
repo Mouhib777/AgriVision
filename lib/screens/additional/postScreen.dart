d2e81a7a2c10d8f764ea7a74cbb2e78fab552c5e
Mouhib777 <mouhibsahbani@gmail.com>
2023-06-12 22:59:01 +0000

    c

--- lib/screens/additional/postScreen.dart
+++ lib/screens/additional/postScreen.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
